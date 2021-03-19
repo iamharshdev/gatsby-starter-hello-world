195f3dfd41aeb4b10bee42fba595b8829170d7ef
Jay Mehta <jake2001007@gmail.com>
2021-03-19 10:07:13 +0530

    changed opacity to1 for footer to wrek

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import "../styles/style.css"
 import "../styles/bootstrap-grid.min.css"
