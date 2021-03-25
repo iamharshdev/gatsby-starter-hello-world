bd32f2e02073c13d8c1bc3c8d8fb3e187dc90388
Jay Mehta <jake2001007@gmail.com>
2021-03-25 11:40:44 +0530

    added 404.js🚀

---
+++ src/pages/404.js
@@ -0,0 +1,39 @@
+import React from "react"
+import "../styles/style.css"
+import "../styles/bootstrap-grid.min.css"
+import "../styles/animsition.min.css"
+import Layout from "../components/Layouts"
+import { Link } from "gatsby"
+
+const ErrorPage = () => {
+  return (
+    <Layout>
+      <div className="wrap">
+        <div className="container">
+          <div className="mar-top-lg">
+            <div className="row">
+              <div className="col-lg-6 col-md-12">
+                <div className="mar-bot-xs">
+                  <div className="page-content-image">
+                    <img src="../img/error404.jpg" alt="Error" />
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-5 offset-lg-1 col-md-10">
+                <div className="page-content">
+                  <h1>404!</h1>
+                  <p className="lead">Seems like you are lost.</p>
+                  <h2>
+                    Lets get you <Link to="/">Home</Link>
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default ErrorPage
