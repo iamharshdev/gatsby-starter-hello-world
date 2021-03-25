015ed765c0c563dfb2b8c941db4052293a41f215
Jay Mehta <jake2001007@gmail.com>
2021-03-25 10:36:59 +0530

    updated gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,30 @@
-const path = require("path")
+// const path = require("path")
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { data } = await graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            html
-            id
-            frontmatter {
-              title
-              date
-              description
-              thumbnail
-            }
-          }
-        }
-      }
-    }
-  `)
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { data } = await graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             html
+//             id
+//             frontmatter {
+//               title
+//               date
+//               description
+//               thumbnail
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  data.allMarkdownRemark.nodes.forEach(node => {
-    actions.createPage({
-      path: "/blog" + node.frontmatter.slug,
-      component: path.resolve("./src/templates/blogPost.js"),
-      context: { slug: node.frontmatter.slug },
-    })
-  })
-}
+//   data.allMarkdownRemark.nodes.forEach(node => {
+//     actions.createPage({
+//       path: "/blog" + node.frontmatter.slug,
+//       component: path.resolve("./src/templates/blogPost.js"),
+//       context: { slug: node.frontmatter.slug },
+//     })
+//   })
+// }
