0a5ee34cd99eb3d70cafaecdeecba238268343bf
Jay Mehta <jake2001007@gmail.com>
2021-03-25 11:45:00 +0530

    Changed date format

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,7 +64,7 @@ export const pageQuery = graphql`
           frontmatter {
             path
             title
-            date(formatString: "YYYY-MM-DD")
+            date(formatString: "DD-MM-YYYY")
             description
             thumbnail
           }
